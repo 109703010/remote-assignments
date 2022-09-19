50cbcbac66e016977530cf11dc985330579b86b8
Andy <andysztzn@gmail.com>
2022-09-19 22:31:47 +0800

    Add js to the web page

---
+++ Week-2/Assignments/app.js
@@ -0,0 +1,11 @@
+const welcome = document.querySelector('header');
+const expand = document.querySelector('footer');
+
+welcome.addEventListener('click', () => {
+    document.querySelector('header p').textContent = "Have a Good Time!";
+})
+
+expand.addEventListener('click', () => {
+    document.getElementById('more').style.display = "flex";
+    // console.log('tap');
+})
